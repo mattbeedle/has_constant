1ce4cb752c759647ec47ecf0e6a84c7c41aaf1fe
mattbeedle <mattbeedle@gmail.com>
2010-07-26 10:18:59 +0200

    Initial commit to has_constant.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'has_constant'
+
+class Test::Unit::TestCase
+end
